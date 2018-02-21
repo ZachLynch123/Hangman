f48ba3eed2250c3c41c985f7be042c54dc020981
Unknown <Zachary.Lynch1337@gmail.com>
2018-02-20 19:08:05 -0800

    commented my code

--- src/com/company/Hangman.java
+++ src/com/company/Hangman.java
@@ -4,7 +4,9 @@ public class Hangman {
     // Main execution file
 
     public static void main(String[] args) {
+        // instantiate the game class
 	    Game game = new Game("answer");
+	    // instantiate the prompter
 	    Prompter prompter = new Prompter(game);
         boolean isHit = prompter.promptForGuess();
         if (isHit) {
