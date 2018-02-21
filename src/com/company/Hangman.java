d026487646b0eb736c79d6b0c6572745ff4b48db
Unknown <Zachary.Lynch1337@gmail.com>
2018-02-20 17:26:30 -0800

    Added simple code to test to see if the hit and miss works

--- src/com/company/Hangman.java
+++ src/com/company/Hangman.java
@@ -5,7 +5,13 @@ public class Hangman {
 
     public static void main(String[] args) {
 	    Game game = new Game("answer");
-
+	    Prompter prompter = new Prompter(game);
+        boolean isHit = prompter.promptForGuess();
+        if (isHit) {
+            System.out.println("We got a hiiiiit!");
+        } else {
+            System.out.println("miss");
+        }
 
     }
 }
